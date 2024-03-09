ff4d47a1c5f9172c943a903b3b6b1995de90c038
Coding-jpg <1305856002@qq.com>
2024-03-09 17:18:10 +1100

    update

--- basic_pandas.py
+++ basic_pandas.py
@@ -15,3 +15,43 @@ import pandas as pd
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
     return products.loc[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y'), ['product_id']]
     
+'''
+183
+'''
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    df = customers[~customers['id'].isin(orders['customerId'])]
+    df = df[['name']].rename(columns={'name': 'Customers'})
+    return df    
+
+'''
+1683
+'''
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    df = tweets[tweets['content'].str.len() > 15]
+    df = df[['tweet_id']]
+    return df
+
+'''
+1873
+'''
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    condition = ~(((employees['employee_id'] % 2) != 0) & (employees['name'].str[0] != 'M'))
+    employees.loc[condition, 'salary'] = 0
+    employees = employees[['employee_id', 'salary']].rename(columns={'salary': 'bonus'}).sort_values(by='employee_id')
+    return employees
+
+'''
+1667
+'''
+import pandas as pd
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    users['name'] = users['name'].str.capitalize()
+    users = users.sort_values(by='user_id')
+    return users
