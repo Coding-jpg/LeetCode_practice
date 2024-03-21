6d9321c18a4ab4d2287262b9349a09593ff565b4
Coding-jpg <1305856002@qq.com>
2024-03-21 23:10:26 +1100

    update

--- sql_practice.py
+++ sql_practice.py
@@ -23,3 +23,26 @@ ORDER BY id
 # Write your MySQL query statement below
 SELECT tweet_id FROM Tweets
 WHERE LENGTH(content) > 15;
+
+'''1378'''
+SELECT unique_id, name
+FROM Employees
+LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id
+
+'''1068'''
+SELECT product_name, year, price
+FROM Sales
+JOIN Product ON Sales.product_id = Product.product_id
+
+'''1581'''
+SELECT customer_id, Count(Visits.visit_id) as count_no_trans
+FROM Visits
+LEFT JOIN Transactions ON Visits.visit_id = Transactions.visit_id
+WHERE Transactions.visit_id IS NULL
+GROUP BY customer_id
+
+'''197'''
+SELECT w2.id
+FROM Weather w1
+JOIN Weather w2 ON datediff(w2.recordDate, w1.recordDate) = 1
+WHERE w2.Temperature > w1.Temperature
