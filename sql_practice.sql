ee4df5291c4b11d74f58d78676e058edd2793bef
Coding-jpg <1305856002@qq.com>
2024-04-05 11:44:49 +1100

    update

--- sql_practice.sql
+++ sql_practice.sql
@@ -258,3 +258,27 @@ SELECT x, y, z,
            ELSE 'No'
        END AS Triangle
 FROM Triangle;
+
+-- 180
+SELECT DISTINCT
+    l1.Num AS ConsecutiveNums
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num
+
+-- 1164
+SELECT DISTINCT product_id, IF(filter_date IS NULL, 10, new_price) AS price
+FROM (
+  SELECT *, RANK() OVER(PARTITION BY product_id ORDER BY filter_date DESC) AS RANKING
+  FROM (
+    SELECT *, IF(change_date > '2019-08-16', NULL, change_date) AS filter_date
+    FROM Products
+  ) T
+) TT
+WHERE TT.RANKING = 1
